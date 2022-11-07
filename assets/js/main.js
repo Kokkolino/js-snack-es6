e9a2a9ad7db40cb527dd0af4d7f8d5970d1339a7
Francesco C <fran461j@outlook.it>
2022-11-07 15:43:26 +0100

    snack 2 uppercase

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,8 +29,30 @@ function snack1(){
         min += -1;
     }
     let myArray2 = myArray.slice(min, max);
-    alert(`i nomi selezionati sono : ${myArray2}`)
-
+    alert(`i nomi selezionati sono : ${myArray2.join(", ")}`)
 }
-
 document.getElementById('snack1').addEventListener('click',snack1);
+
+// snack2
+// studenti
+let students = [
+    { name: 'Marco', id: 213, grades: 78 },
+    { name: 'Paola', id: 110, grades: 96 },
+    { name: 'Andrea', id: 250, grades: 48 },
+    { name: 'Gaia', id: 145, grades: 74 },
+    { name: 'Luigi', id: 196, grades: 68 },
+    { name: 'Piero', id: 102, grades: 50 },
+    { name: 'Francesca', id: 120, grades: 84 },
+];
+// funzione
+function snack2(){
+    // uppercase
+  let studentsName = [];
+  for(let key in students){
+    studentsName.push(students[key].name.toUpperCase());
+  }
+  alert(`la lista nomi é: ${studentsName.join(', ')}`);
+
+  
+}
+document.getElementById('snack2').addEventListener('click',snack2);
